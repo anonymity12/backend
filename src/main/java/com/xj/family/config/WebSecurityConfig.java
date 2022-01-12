cef3f6da60d05cde3396c115c43ef5f5054342d9
Your Name <you@example.com>
2022-01-12 18:26:02 +0800

    开启了in memory认证登录能力

--- src/main/java/com/xj/family/config/WebSecurityConfig.java
+++ src/main/java/com/xj/family/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -30,44 +31,54 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("xiaojia").password(new BCryptPasswordEncoder().encode("666666")).roles("vip2", "vip3")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("666666")).roles("vip1", "vip2", "vip3")
+                .and()
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("666666")).roles("vip1", "vip2");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/category/all").authenticated()
-                .antMatchers("/admin/**","/reg").hasRole("超级管理员")///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
-                .anyRequest().authenticated()//其他的路径都是登录后即可访问
+                .antMatchers("/admin/**", "/reg").hasRole("超级管理员")/// admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
+                .anyRequest().authenticated()// 其他的路径都是登录后即可访问
                 .and().formLogin().loginPage("/login_page").successHandler(new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                System.out.println("ttt>>> login is ok!");
-                httpServletResponse.setContentType("application/json;charset=utf-8");
-                PrintWriter out = httpServletResponse.getWriter();
-                out.write("{\"status\":\"success\",\"msg\":\"登录成功\"}");
-                out.flush();
-                out.close();
-            }
-        })
-                .failureHandler(new AuthenticationFailureHandler() {
                     @Override
-                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                            HttpServletResponse httpServletResponse, Authentication authentication)
+                            throws IOException, ServletException {
                         System.out.println("ttt>>> login is ok!");
                         httpServletResponse.setContentType("application/json;charset=utf-8");
                         PrintWriter out = httpServletResponse.getWriter();
-                        out.write("{\"status\":\"error\",\"msg\":\"登录失败\"}");
+                        out.write("{\"status\":\"success\",\"msg\":\"登录成功\"}");
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                            HttpServletResponse httpServletResponse, AuthenticationException e)
+                            throws IOException, ServletException {
+                        System.out.println("ttt>>> login is failed!!");
+                        httpServletResponse.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        out.write("{\"status\":\"error\",\"msg\":\"登录失败，您哪位啊\"}");
                         out.flush();
                         out.close();
                     }
                 }).loginProcessingUrl("/login")
                 .usernameParameter("username").passwordParameter("password").permitAll()
-                .and().logout().permitAll().and().csrf().disable().exceptionHandling().accessDeniedHandler(getAccessDeniedHandler());
+                .and().logout().permitAll().and().csrf().disable().exceptionHandling()
+                .accessDeniedHandler(getAccessDeniedHandler());
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/blogimg/**","/index.html","/static/**");
+        web.ignoring().antMatchers("/blogimg/**", "/index.html", "/static/**");
     }
 
     @Bean
