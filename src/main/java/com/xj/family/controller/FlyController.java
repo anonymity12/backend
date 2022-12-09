fcdb492b5ce73bd2363cc1191deba115fbd8b300
ttAtTencentCloud <1135175824@qq.com>
2022-12-09 10:12:47 +0800

    fly module: remove useless import of mapper for controller

--- src/main/java/com/xj/family/controller/FlyController.java
+++ src/main/java/com/xj/family/controller/FlyController.java
@@ -1,6 +1,5 @@
 package com.xj.family.controller;
 
-import com.xj.family.mapper.FlyItemMapper;
 import com.xj.family.bean.FlyItem;
 import com.xj.family.bean.RespBean;
 import com.xj.family.service.FlyService;
