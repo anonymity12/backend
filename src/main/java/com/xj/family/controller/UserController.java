78b0065628c8020b771d0caa2da2b0222efb80e3
ttAtTencentCloud <1135175824@qq.com>
2022-12-10 14:48:14 +0800

    add: basic for usercontroller : valid parent

--- src/main/java/com/xj/family/controller/UserController.java
+++ src/main/java/com/xj/family/controller/UserController.java
@@ -21,4 +21,13 @@ public class UserController {
     public User getInfo(@PathVariable("username") String name) {
         return userService.getUserInfoByName(name);
     }
+
+    @PostMapping("/validParent")
+    public RespBean validParent(@RequestBody ValidParam item) {
+        if (userService.validParent(item)) { // todo : validParent 2022-12-10 14:47:46
+            return RespBean.ok("验证通过");
+        } else {
+            return RespBean.error("验证不通过");
+        }
+    }
 }
