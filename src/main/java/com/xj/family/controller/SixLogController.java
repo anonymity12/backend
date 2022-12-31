6792acb42df633a24c60457fceda4cc30fb7689c
ttAtTencentCloud <1135175824@qq.com>
2022-12-31 23:51:49 +0800

    can upload img now

--- src/main/java/com/xj/family/controller/SixLogController.java
+++ src/main/java/com/xj/family/controller/SixLogController.java
@@ -3,10 +3,15 @@ package com.xj.family.controller;
 import com.xj.family.service.SixLogService;
 import com.xj.family.bean.SixLog;
 import com.xj.family.bean.RespBean;
+import com.xj.family.config.Constants;
+import com.xj.family.utils.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Date;
 
@@ -43,5 +48,26 @@ public class SixLogController {
             return RespBean.ok("this is the totalAmount in obj", Integer.valueOf(ret));
         else 
             return RespBean.error("failed to get sixlog total amount");
+    }
+    
+    @CrossOrigin(origins = Constants.FRONT_URL_DEV, methods = {RequestMethod.DELETE,
+                    RequestMethod.GET,RequestMethod.POST,RequestMethod.OPTIONS}
+                , allowedHeaders = "*", allowCredentials="true")
+    @PostMapping("/covers")
+    public String coversUpload(MultipartFile file) {
+        String folder = "/home/tt/code/CodeForFamily/backend/img_upload/";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename()
+                .substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            String imgURL = Constants.SERVER_URL + "/api/img/" + f.getName();
+            return imgURL;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
