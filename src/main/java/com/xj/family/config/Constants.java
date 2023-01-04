7f640ff4c767974758bd32c7189fe0179aff3832
ttAtTencentCloud <1135175824@qq.com>
2023-01-04 14:39:03 +0800

    add constants

---
+++ src/main/java/com/xj/family/config/Constants.java
@@ -0,0 +1,7 @@
+package com.xj.family.config;
+
+public class Constants {
+    public static final String SERVER_URL = "http://101.43.166.211:8081";
+    public static final String FRONT_URL_DEV = "http://101.43.166.211:8091";// http://101.43.166.211:8091
+    public static final String FRONT_URL_PRD = "http://101.43.166.211:8089";
+}
