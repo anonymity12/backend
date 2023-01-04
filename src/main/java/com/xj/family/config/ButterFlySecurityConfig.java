fe679ea9df0eda5198a30773ab2d907ec41d5bda
ttAtTencentCloud <1135175824@qq.com>
2023-01-04 14:37:21 +0800

    view img bug fixed

--- src/main/java/com/xj/family/config/ButterFlySecurityConfig.java
+++ src/main/java/com/xj/family/config/ButterFlySecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -22,6 +23,23 @@ public class ButterFlySecurityConfig implements WebMvcConfigurer {
         //添加拦截器
         registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/api/**")
                 //放行路径，可以添加多个
-                .excludePathPatterns("/api/login");
+                .excludePathPatterns("/api/login", "/api/sixlog/covers","/api/img/**");// append sixlog/covers at 2022-12-31 23:15:39
     }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/img/**").addResourceLocations("file:" + "/home/tt/code/CodeForFamily/backend/img_upload/");
+    }
+  //   // fix element ui request bug
+  //   @Override
+  // public void addCorsMappings(CorsRegistry registry) {
+
+  //   registry.addMapping("/api/img/**")
+  //     .allowedOrigins(Constants.FRONT_URL_DEV)
+  //     .allowedMethods("GET", "POST")
+  //     .allowCredentials(true).maxAge(3600);
+
+  //   // Add more mappings...
+  // }
 }
