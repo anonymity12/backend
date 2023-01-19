8a35756dd6d8e4e09d5a3cc2223e0278b2ac90d7
anonymity12 <ztt1995120@gmail.com>
2023-01-19 20:37:31 +0800

    todo: do we need refactor LoginVo

--- src/main/java/com/xj/family/vo/LoginVO.java
+++ src/main/java/com/xj/family/vo/LoginVO.java
@@ -13,6 +13,8 @@ import java.io.Serializable;
  * @Author: 三分恶
  * @Date: 2021/1/18
  * @Description: 登录VO
+  tt todo 2023-01-19 20:26:17 : we need move it to bean/vo folder
+  todo 2023-01-19 20:32:54 try not use User inside as a member; may has serialization error, but test will tell us
  **/
 
 public class LoginVO implements Serializable {
