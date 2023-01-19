b0689a3b2c37d5dc6054188dfa710925a8a8c4fb
anonymity12 <ztt1995120@gmail.com>
2023-01-19 20:36:07 +0800

    add string constant for session loginUser key

--- src/main/java/com/xj/family/config/Constants.java
+++ src/main/java/com/xj/family/config/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String SERVER_URL = "http://101.43.166.211:8081";
     public static final String FRONT_URL_DEV = "http://101.43.166.211:8091";// http://101.43.166.211:8091
     public static final String FRONT_URL_PRD = "http://101.43.166.211:8089";
+    public static final String LOGIN_USER = "loginUser";
 }
