ddd4ba94552ea25e98405148b629188e6dc0e941
anonymity12 <ztt1995120@gmail.com>
2023-01-19 20:35:34 +0800

    login controller will set session now

--- src/main/java/com/xj/family/controller/LoginController.java
+++ src/main/java/com/xj/family/controller/LoginController.java
@@ -7,6 +7,7 @@ package com.xj.family.controller;
 import com.xj.family.dto.LoginDTO;
 import com.xj.family.result.Result;
 import com.xj.family.service.LoginService;
+import com.xj.family.config.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,12 +25,21 @@ public class LoginController {
     LoginService loginService;
 
     @PostMapping(value = "/api/login")
-    public Result login(@RequestBody LoginDTO loginDTO) {
+    public Result login(@RequestBody LoginDTO loginDTO, HttpSession session) {
         Result ret = loginService.login(loginDTO);
+        if (ret.getCode() != 200) {
+            // wrong in login service, so just return 
+            return ret;
+        } else {
+            // this session attr contain whole loginVo{id, token, User}
+            session.setAttribute(Constants.LOGIN_USER, ret.getData());
+            return ret;
+        }
+        
 	/*
 	hashOps = redisTemplate.opsForHMap();	
 	hashOps.put(ret.getToken(), ret.getUserInfo());// UserInfo:(at least) contains: name, userId, cname, 
+    maybe: use session is enough, for {username, cname, name, userId}
 	*/
-	return ret;
     }
 }
