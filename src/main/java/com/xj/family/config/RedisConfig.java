194db6f848cd9e6a83843a0d7a0e08e11a35bad3
anonymity12 <ztt1995120@gmail.com>
2023-02-05 15:44:01 +0800

    fix redis serialization bug with redis config

---
+++ src/main/java/com/xj/family/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.xj.family.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();// or  Jackson2JsonRedisSerializer
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(redisSerializer);
+        template.setValueSerializer(redisSerializer);
+        template.setHashKeySerializer(redisSerializer);
+        template.setHashValueSerializer(redisSerializer);
+
+        return template;
+    }
+}
