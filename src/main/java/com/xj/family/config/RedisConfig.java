53cfe55bcb2a0f0feb86fc0c2cdc465649c9b514
anonymity12 <ztt1995120@gmail.com>
2023-02-05 15:50:42 +0800

    use GenericJackson2JsonRedisSerializer instead

--- src/main/java/com/xj/family/config/RedisConfig.java
+++ src/main/java/com/xj/family/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -12,7 +14,9 @@ public class RedisConfig {
     @Bean(name = "redisTemplate")
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();// or  Jackson2JsonRedisSerializer
+        RedisSerializer redisSerializer = new GenericJackson2JsonRedisSerializer();// or  Jackson2JsonRedisSerializer
+        // StringRedisSerializer cause 500 err:
+        //     java.util.ArrayList cannot be cast to java.lang.String
         template.setConnectionFactory(factory);
         template.setKeySerializer(redisSerializer);
         template.setValueSerializer(redisSerializer);
