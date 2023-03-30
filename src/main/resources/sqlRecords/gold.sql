6235a0566dc2bb81bb5f7bf458954eaf7fcffcf1
anonymity12 <ztt1995120@gmail.com>
2023-03-30 21:57:13 +0800

    move some sql files to better place

---
+++ src/main/resources/sqlRecords/gold.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `gold` (
+  `owner_id` int(11) NOT NULL DEFAULT '1' COMMENT 'whom gold amount belongs to',
+  `amount` int(11) NOT NULL DEFAULT '5' COMMENT 'his balance'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
