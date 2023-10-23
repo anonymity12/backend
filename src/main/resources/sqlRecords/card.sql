6cd1bf2752d08e36a363bf47015f45d98a573653
anonymity12 <ztt1995120@gmail.com>
2023-10-23 21:53:56 +0800

    update card sql

--- src/main/resources/sqlRecords/card.sql
+++ src/main/resources/sqlRecords/card.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `card_series` (
   `id` int(11) NOT NULL COMMENT '系列ID',
   `name` varchar(255) NOT NULL COMMENT '系列名称',
-  `desc` varchar(255) DEFAULT NULL COMMENT '系列描述'
+  `desc` varchar(255) DEFAULT NULL COMMENT '系列描述',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `card_template` (
@@ -9,15 +10,17 @@ CREATE TABLE IF NOT EXISTS `card_template` (
   `name` varchar(255) DEFAULT NULL,
   `series` int(11) DEFAULT 0 COMMENT '这个卡片属于什么系列',
   `image_url` varchar(255) DEFAULT NULL,
-  `base_price` int(11) NOT NULL DEFAULT 100 COMMENT '卡片的基础价格'
-  `desc` varchar(255) DEFAULT '一张价值连城的卡片，前提是经过了你的努力' COMMENT '卡片描述'
+  `base_price` int(11) NOT NULL DEFAULT 100 COMMENT '卡片的基础价格',
+  `desc` varchar(255) DEFAULT '一张价值连城的卡片，前提是经过了你的努力' COMMENT '卡片描述',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `card_instance` (
-  `id` int(11) NOT NULL,
-  `template_id` int(11) NOT NULL,
-  `hard_work_rate` int(11) DEFAULT 1 COMMENT '卡片上附加的努力率，用于计算实际最终价值的因子'
-  `owner` int(11) NOT NULL DEFAULT 0 COMMENT '这个卡片当前属于谁',
-  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '卡片的状态0:入库，1:正在炼制'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `card_instance` (
+ `id` int(11) NOT NULL,
+ `template_id` int(11) NOT NULL,
+ `hard_work_rate` int(11) DEFAULT '1' COMMENT '卡片上附加的努力率，用于计算实际最终价值的因子',
+ `owner` int(11) NOT NULL DEFAULT '0' COMMENT '这个卡片当前属于谁',
+ `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '卡片的状态0:入库，1:正在炼制',
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
