11114830b17ca1c747f2a94e55418437aafb04c1
anonymity12 <ztt1995120@gmail.com>
2023-10-28 08:03:35 +0800

    CardInstanceMapper.java: better code wiring

--- src/main/java/com/xj/family/mapper/CardInstanceMapper.java
+++ src/main/java/com/xj/family/mapper/CardInstanceMapper.java
@@ -9,13 +9,19 @@ import java.util.List;
 
 public interface CardInstanceMapper {
     // ----------------------- business methods -----------------------
-    /* called when users trade,
+    CardTemplate getCardTemplate(int cardInstanceId);
+    List<CardInstance> listUserCards(int userId); // called when one user want to see all his/her cards
+    CardInstance getUserMainCard(@Param("owner")Integer owner);
+    int switchMainCard(int newCardId, int userId); // called when someone decide a card to refine
+    int clearUserMainCard(Integer owner);
+    int setUserMainCard(@Param("cardInstanceId") int cardInstanceId);
+    int getCardOwner(int cardInstanceId);
+    int setCardOwner(int cardInstanceId, int cardTo);
+    /*
+       called when users trade,
        but we need gold system to support, later
      */
     int tradeCard(int fromUserId, int toUserId);
-    List<CardInstance> listUserCards(int userId); // called when one user want to see all his/her cards
-    int switchMainCard(int newCardId, int userId); // called when someone decide a card to refine
-
 
     // ----------------------- basic crud methods -----------------------
     int createCardInstance(int userId, int cardTemplateId);
@@ -23,16 +29,4 @@ public interface CardInstanceMapper {
     int upgradeCard(int cardId); // called when user finish a task
     int downgradeCard(int cardId); // called when user undone a task
     int deleteCardInstance(int cardId);
-
-    int clearUserMainCard(Integer owner);
-
-    CardInstance getUserMainCard(@Param("owner")Integer owner);
-
-    int setUserMainCard(@Param("cardInstanceId") int cardInstanceId);
-
-    int getCardOwner(int cardInstanceId);
-
-    int setCardOwner(int cardInstanceId, int cardTo);
-
-    CardTemplate getCardTemplate(int cardInstanceId);
 }
