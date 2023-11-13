6d602361cf65511f5be762c326c9b60e61af9252
anonymity12 <ztt1995120@gmail.com>
2023-11-13 08:39:13 +0800

    CardSquareController.java: just add

---
+++ src/main/java/com/xj/family/controller/CardSquareController.java
@@ -0,0 +1,26 @@
+package com.xj.family.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/card-square")
+public class CardSquareController {
+    /*
+    @Autowired
+    CardShopService cardShopService;
+
+    @GetMapping("/listAll")
+
+    @GetMapping("/getTop3")
+
+    @GetMapping("/query/{keyword}")
+
+    @PostMapping("/thumbs-up/{cardId}")
+
+    @PostMapping("/send-buy-request/{cardId}")
+
+     */
+}
