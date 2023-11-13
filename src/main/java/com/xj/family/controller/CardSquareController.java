e12b45bd51521bcbe53e2c217acd9d83f6d04d6f
anonymity12 <ztt1995120@gmail.com>
2023-11-13 21:46:07 +0800

    CardSquareController.java: add one method: getAll and it's service code

--- src/main/java/com/xj/family/controller/CardSquareController.java
+++ src/main/java/com/xj/family/controller/CardSquareController.java
@@ -1,20 +1,34 @@
 package com.xj.family.controller;
 
+import com.xj.family.bean.RespBean;
+import com.xj.family.bean.vo.CardVo;
+import com.xj.family.interceptor.LoginInterceptor;
+import com.xj.family.service.CardSquareService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/card-square")
 public class CardSquareController {
-    /*
     @Autowired
-    CardShopService cardShopService;
+    CardSquareService cardSquareService;
 
     @GetMapping("/listAll")
+    public RespBean listAll() {
+        Integer owner = LoginInterceptor.threadLocalUserId.get();
+        List<CardVo> allSquareCards = cardSquareService.getAllSquareCards();
+        return RespBean.ok("OK", allSquareCards);
+    }
+    /*
 
     @GetMapping("/getTop3")
+
 
     @GetMapping("/query/{keyword}")
 
---
+++ src/main/java/com/xj/family/service/CardSquareService.java
@@ -0,0 +1,19 @@
+package com.xj.family.service;
+import com.xj.family.bean.vo.CardVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CardSquareService {
+
+    public List<CardVo> getAllSquareCards() {
+        // mapper get all cards
+        // sort it by price
+        // finally return it;
+        return null;
+    }
+}
