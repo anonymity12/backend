d4ce6e6f56af5fee3f52b705ae55977291ef12da
anonymity12 <ztt1995120@gmail.com>
2023-12-14 22:27:33 +0800

    update sql comment for gold

--- src/main/resources/sqlRecords/gold.sql
+++ src/main/resources/sqlRecords/gold.sql
@@ -1,4 +1,4 @@
 CREATE TABLE `gold` (
-  `owner_id` int(11) NOT NULL DEFAULT '1' COMMENT 'whom gold amount belongs to',
+  `owner_id` int(11) NOT NULL DEFAULT '1' COMMENT 'whom gold belongs to',
   `amount` int(11) NOT NULL DEFAULT '5' COMMENT 'his balance'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
