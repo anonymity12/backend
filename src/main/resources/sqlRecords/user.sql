7ac3446e61a05b167b80d6779bb189c1fcba106f
ttAtTencentCloud <1135175824@qq.com>
2024-12-07 12:20:32 +0800

    just sql doc update: user need a group id, but we do it later

--- src/main/resources/sqlRecords/user.sql
+++ src/main/resources/sqlRecords/user.sql
@@ -1,3 +1,23 @@
+-- generate by tt at 2024 1207
+--  show create table user;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
+  `name` varchar(32) DEFAULT NULL COMMENT '英文名缩写',
+  `intro` varchar(255) NOT NULL DEFAULT '点击头像可以自定义' COMMENT '介绍',
+  `userface` varchar(255) DEFAULT 'https://picgorepo.oss-cn-beijing.aliyuncs.com/2023-05-17-10-23-29.png' COMMENT '头像',
+  `phone` char(11) DEFAULT NULL COMMENT '手机号码',
+  `address` varchar(64) DEFAULT NULL COMMENT '联系地址',
+  `enabled` tinyint(1) DEFAULT '1',
+  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `cname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4    
+
+-- (2024 1207) todo we need a group_id 
+
+
+
 -- phpMyAdmin SQL Dump
 -- version 4.4.15.10
 -- https://www.phpmyadmin.net
